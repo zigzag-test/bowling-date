7fb05676ac2b0e389992083cfaad209ad02a779d
huskyhoochu <dfg1499@gmail.com>
2018-11-12 16:36:24 +0900

    각 수행 요소의 함수화 작업

--- bowling/bowling.js
+++ bowling/bowling.js
@@ -1,46 +1,68 @@
-function countBowlingScore(frames) {
-  const framesArray = frames.split('');
-  const convertedStrikeValue = framesArray.map(item => item === 'A' ? '10' : item);
-  const convertedIntValue = convertedStrikeValue.map(item => parseInt(item, 10));
-  const resultArray = [];
+const numOfShotToEvalScore = {
+  strike: 3,
+  spare: 3,
+  open: 2,
+};
+
+function convertInputToIntArr(input) {
+  const rawArr = input.split('');
+  const convertedStrikeValueArr = rawArr.map(value => value === 'A' ? '10' : value);
+  return convertedStrikeValueArr.map(value => parseInt(value, 10));
+}
+
+function isEnoughToEvalScore(inputArr, head, numOfShotToEvalScore) {
+  const scoreArr = inputArr.slice(head, head + numOfShotToEvalScore);
+  return scoreArr.length === numOfShotToEvalScore;
+}
+
+function getPrevScore(resultArr) {
+  return resultArr.slice(-1)[0] ? resultArr.slice(-1)[0] : 0;
+}
+
+function getCurrentScore(inputArr, head, numOfShotToEvalScore) {
+  const scoreArr = inputArr.slice(head, head + numOfShotToEvalScore);
+  return scoreArr.reduce((prev, next) => prev + next);
+}
+
+function countBowlingScore(input) {
+  const inputArr = convertInputToIntArr(input);
+  const resultArr = [];
 
   let head = 0;
   let frame = 0;
 
-  while (head < convertedIntValue.length) {
-    // 10프레임을 넘기지 않는다
+  while (head < inputArr.length) {
+    // 10 프레임을 넘기지 않는다
     if (frame >= 10) {
       break;
     }
 
+    const prevScore = getPrevScore(resultArr);
+
     // 스트라이크
-    if (convertedIntValue[head] === 10) {
-      if (convertedIntValue.slice(head, head + 3).length < 3) {
+    if (inputArr[head] === 10) {
+      if (!isEnoughToEvalScore(inputArr, head, numOfShotToEvalScore.strike)) {
         break;
       }
 
-      const prevCount = resultArray.slice(-1)[0] ? resultArray.slice(-1)[0] : 0;
-      const countOfFrame = convertedIntValue.slice(head, head + 3).reduce((prev, next) => prev + next);
-      const totalCount = prevCount + countOfFrame;
+      const totalScore = prevScore + getCurrentScore(inputArr, head, numOfShotToEvalScore.strike);
 
-      resultArray.push(totalCount);
-      head += 1;
+      resultArr.push(totalScore);
+      head += 1; // 현재 샷이 스트라이크면 다음 프레임은 바로 다음 샷이므로 head를 1만 전진시킨다
       frame += 1;
 
       continue;
     }
 
     // 스페어
-    if (convertedIntValue[head] + convertedIntValue[head + 1] === 10) {
-      if (convertedIntValue.slice(head, head + 3).length < 3) {
+    if (inputArr[head] + inputArr[head + 1] === 10) {
+      if (!isEnoughToEvalScore(inputArr, head, numOfShotToEvalScore.spare)) {
         break;
       }
 
-      const prevCount = resultArray.slice(-1)[0] ? resultArray.slice(-1)[0] : 0;
-      const countOfFrame = convertedIntValue.slice(head, head + 3).reduce((prev, next) => prev + next);
-      const totalCount = prevCount + countOfFrame;
+      const totalScore = prevScore + getCurrentScore(inputArr, head, numOfShotToEvalScore.spare);
 
-      resultArray.push(totalCount);
+      resultArr.push(totalScore);
       head += 2;
       frame += 1;
 
@@ -48,16 +70,14 @@ function countBowlingScore(frames) {
     }
 
     // 오픈
-    if (convertedIntValue[head] + convertedIntValue[head + 1] < 10) {
-      if (convertedIntValue.slice(head, head + 2).length < 2) {
+    if (inputArr[head] + inputArr[head + 1] < 10) {
+      if (!isEnoughToEvalScore(inputArr, head, numOfShotToEvalScore.open)) {
         break;
       }
 
-      const prevCount = resultArray.slice(-1)[0] ? resultArray.slice(-1)[0] : 0;
-      const countOfFrame = convertedIntValue.slice(head, head + 2).reduce((prev, next) => prev + next);
-      const totalCount = prevCount + countOfFrame;
+      const totalScore = prevScore + getCurrentScore(inputArr, head, numOfShotToEvalScore.open);
 
-      resultArray.push(totalCount);
+      resultArr.push(totalScore);
       head += 2;
       frame += 1;
 
@@ -65,19 +85,19 @@ function countBowlingScore(frames) {
     }
 
     // 에러: 두 핀의 합이 10 초과
-    if (convertedIntValue[head] + convertedIntValue[head + 1] > 10) {
+    if (inputArr[head] + inputArr[head + 1] > 10) {
       return 'error';
     }
   }
 
-  return resultArray;
+  return resultArr;
 }
 
-const inputObj = {
+const inputExample = {
   perfect: 'AAAAAAAAAAAA',
   normal: '82A900519A',
   longest: '11111111111111111111',
   error: '82A930519A',
 };
 
-console.log(countBowlingScore(inputObj.normal));
+console.log(countBowlingScore(inputExample.error));
