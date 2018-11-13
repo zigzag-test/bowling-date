0984a680290d8c4fa199a4a6fa917fb801c215d0
huskyhoochu <dfg1499@gmail.com>
2018-11-13 12:39:20 +0900

    메인 함수 export

--- bowling/bowling.js
+++ bowling/bowling.js
@@ -1,15 +1,6 @@
 const utils = require('./utils');
 
-const countBowlingScore = (input) => {
+exports.countBowlingScore = (input) => {
   const inputArr = utils.convertInputToIntArr(input);
   return utils.loopInputArr(inputArr);
 };
-
-const inputExample = {
-  perfect: 'AAAAAAAAAAAA',
-  incomplete: '82A900519A',
-  fullGame: '817390A63A82AA73A',
-  error: '82A930519A',
-};
-
-console.log(countBowlingScore(inputExample.fullGame));
